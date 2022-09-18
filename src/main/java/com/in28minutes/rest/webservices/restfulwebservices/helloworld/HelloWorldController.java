736a650583a939729d2cdb4f6f27696668a94467
techprograms <tech.gpr@gmail.com>
2022-09-18 21:17:08 +0530

    added

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -8,19 +8,17 @@ public class HelloWorldController {
 
 	@GetMapping(path = "/hello-world")
 	public String helloWorld() {
-		//returning a String
-		//xyz
-		//abc
+		// returning a String
+		// xyz
+		// abc
 		return "Hello World Controller";
 	}
-	
-<<<<<<< HEAD
+
 	public String sayHello() {
 		return "Hello Sir";
 	}
-=======
- public String getMessage() {
- 	return "Hi";
- }
->>>>>>> 9a096d0b7b9141e1288e4cb878adcbbc8d8b7f8d
+
+	public String getMessage() {
+		return "Hi";
+	}
 }
