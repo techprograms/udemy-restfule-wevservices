6b86a5b8f520acf91860462ddff55d13cb93b564
techprograms <39184153+techprograms@users.noreply.github.com>
2022-09-19 16:29:22 +0530

    Update HelloWorldBean.java

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldBean.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldBean.java
@@ -3,7 +3,7 @@ package com.in28minutes.rest.webservices.restfulwebservices.helloworld;
 public class HelloWorldBean {
 
 	private String message;
-
+        //constructor
 	public HelloWorldBean(String message) {
 		this.message = message;
 	}
