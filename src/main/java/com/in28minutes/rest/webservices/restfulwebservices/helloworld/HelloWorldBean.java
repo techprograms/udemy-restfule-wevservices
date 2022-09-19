b59d86f419da14c44e5b31c49a3cd7f86499473d
techprograms <tech.gpr@gmail.com>
2022-09-19 16:27:48 +0530

    added getter, setter and toString() method

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldBean.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldBean.java
@@ -5,7 +5,20 @@ public class HelloWorldBean {
 	private String message;
 
 	public HelloWorldBean(String message) {
-		super();
 		this.message = message;
 	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	@Override
+	public String toString() {
+		return "HelloWorldBean [message=" + message + "]";
+	}
+
 }
