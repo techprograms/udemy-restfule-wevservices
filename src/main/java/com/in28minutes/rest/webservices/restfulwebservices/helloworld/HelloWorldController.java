388b61ed738253aaa698a53abaacabef598fd2ef
techprograms <39184153+techprograms@users.noreply.github.com>
2022-09-19 16:21:46 +0530

    Update HelloWorldController.java

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -11,18 +11,8 @@ public class HelloWorldController {
 		return "Hello World Controller";
 	}
 	
-<<<<<<< HEAD
 	@GetMapping(path = "/hello-world-bean")
 	public HelloWorldBean helloWorldBean() {
 		return new HelloWorldBean("Hello World Bean");
 	}
-
-=======
-	public String sayHello() {
-		return "Hello Sir";
-	}
- public String getMessage() {
- 	return "Hi";
- }
->>>>>>> e5c24e2c2c64c6939610d2c7e04524496a92dc43
 }
